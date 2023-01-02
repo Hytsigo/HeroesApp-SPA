64d0b8cea7cec0a9cf252d29591f2545c7434700
Hytsigo <hytsigo1@gmail.com>
2023-01-01 20:16:39 -0500

    first commit

---
+++ src/auth/pages/LoginPages.jsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router";
+
+export const LoginPages = () => {
+
+  const navigate = useNavigate();
+
+  const onLogin = () => {
+    navigate("/", {
+      replace: true
+  });
+  }
+
+  return (
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+
+      <button className="btn btn-primary"
+      onClick={ onLogin}>
+        Login
+      </button>
+    </div>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter} from "react-router-dom";
+
+import { HeoresApp } from './HeoresApp'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <HeoresApp/>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
